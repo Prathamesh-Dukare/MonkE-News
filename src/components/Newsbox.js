fd516a545c73f670ee1fb75a1e2622eae10fded2
Prathamesh Dukare <prathameshdukaremail@gmail.com>
2021-10-29 14:42:31 +0530

    Fixed : File Structure and Cleanup

---
+++ src/components/Newsbox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Newsbox extends Component {
+    render() {
+        return (
+            <div>
+                Box
+            </div>
+        )
+    }
+}
